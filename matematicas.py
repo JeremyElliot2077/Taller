35eed92408b604626fee0d585948d315a3fff7fa
JeremyElliot2077 <136006477+JeremyElliot2077@users.noreply.github.com>
2023-06-15 20:42:30 -0500

    Add files via upload

---
+++ matematicas.py
@@ -0,0 +1,10 @@
+def sumar(a, b):
+    return a + b
+def restar (a, b):
+    return a - b
+def dividir(a, b):
+    return a / b
+def multiplicar(a, b):
+    return a * b
+def distancia(x1, y1, x2, y2):
+    return x2**2 - x1**2 + y2**2 - y1**2
---
+++ programa.py
@@ -0,0 +1,7 @@
+from matematicas import sumar, restar, multiplicar, dividir, distancia
+
+print(sumar( 5, 5))
+print(restar(18, 4))
+print(multiplicar(2, 8))
+print(dividir(84, 4))
+print(distancia(3, 5, 8, 4))
